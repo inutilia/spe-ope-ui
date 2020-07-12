b1d84ef6894c1f2f1403f909aa8dd1d7ca7b9290
ryoksmt <ryo.arima.ksmt@gmail.com>
2020-07-12 21:31:54 +0900

    add vuesax

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>{{x1}}, {{y1}}</span>
+    <svg id="myCanvas" width="300" height="300" @mousemove="showCoordinates">
+      <line :x1=this.x1 :y1=this.y1 :x2=this.y1 :y2=this.x1 stroke="black"></line>
+    </svg>
+    <vs-button>Hello World</vs-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data() {
+    return {
+      canvas: null,
+      x1: 0,
+      y1: 0
+    }
+  },
+  methods: {
+    showCoordinates(e) {
+      this.x1 = e.offsetX;
+      this.y1 = e.offsetY;
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #myCanvas {
+    background-color: grey;
+  }
 </style>
+
